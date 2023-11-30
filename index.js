98417cd22c6c6e11bf40d91ddde98c4e9141540d
4Tipy <leosteven2010@hotmail.com>
2023-11-30 21:23:54 +0100

    bucle nMinasAdyacentes

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function iniciarPartida() {
 }
 
 function crearTabla() {
+
     let tablaHTML = `<table border="1px solid black">`;
     for (let i = 1; i <= numFilas; i++) {
         tablaHTML += '<tr>';
@@ -73,18 +74,54 @@ function esMina(fila, columna) {
 }
 
 function abrirCasilla(fila, columna) {
-    casilla = document.getElementById(`td-${fila}-${columna}`);
+    let casilla = document.getElementById(`td-${fila}-${columna}`);
     let imagen = document.getElementById(`img-${fila}-${columna}`);
+    let nMinesAdjacentes = 0;
 
-    if (esMina(fila,columna)) {
-        imagen.src = "./imagenes/mina20px.jpg";
-        console.log("has encontrado una mina");
+    if (esMina(fila, columna) != true) {
+                // Calcular el número de minas adyacentes
+                for (let i = fila - 1; i <= fila + 1; i++) {
+                    for (let j = columna - 1; j <= columna + 1; j++) {
+                        if (i >= 1 && i <= numFilas && j >= 1 && j <= numColumnas && !(i === fila && j === columna)) {
+                            let casillaAdyacente = document.getElementById(`td-${i}-${j}`);
+                            if (casillaAdyacente != null && casillaAdyacente.dataset.mina === "true") {
+                                nMinesAdjacentes++;
+                            }
+                        }
+                    }
+                }
+        
+                // Mostrar el número de minas adyacentes en la casilla
+                if (nMinesAdjacentes > 0) {
+                    casilla.innerHTML = nMinesAdjacentes;
+                } else {
+                    casilla.innerHTML = "0"; // Puedes dejarlo vacío si no hay minas adyacentes
+                }
+        
     } else {
-        casilla.innerHTML="0";
-        console.log("eres aweonao")
+        imagen.src = "./imagenes/mina20px.jpg";
+        console.log("Has encontrado una mina");
+        return imagen;
     }
 }
-function setMinesAdjacents(fila,columna,nMinesAdjacentes){
+function setMinesAdjacents(fila, columna, nMinesAdjacentes) {
+    for (let i = fila - 1; i <= fila + 1; i++) {
+        for (let j = columna - 1; j <= columna + 1; j++) {
+            // Verifica que no estés fuera de los límites de la tabla
+            if (i >= 1 && i <= numFilas && j >= 1 && j <= numColumnas) {
+                // Verifica si la casilla actual no es la misma que la original
+                if (!(i === fila && j === columna)) {
+                    // Obtiene la casilla adyacente
+                    let casillaAdyacente = document.getElementById(`td-${i}-${j}`);
+
+                    // Verifica si la casilla adyacente es una mina
+                    if (casillaAdyacente != null && casillaAdyacente.dataset.mina === "true") {
+                        nMinesAdjacentes++;
+                    }
+                }
+            }
+        }
+    }
 
 }
 
